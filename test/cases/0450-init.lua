05b0c22472f90c89800a48a3e7de483ac5e6dd1d
Alexander Altshuler <altexy@gmail.com>
2012-06-01 13:45:31 +0400

    cases/0450-init: add test for init

---
+++ test/cases/0450-init.lua
@@ -0,0 +1,8 @@
+--------------------------------------------------------------------------------
+-- 0450-init.lua:
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+-- TODO: Add description!
+-- TODO: Write tests!
