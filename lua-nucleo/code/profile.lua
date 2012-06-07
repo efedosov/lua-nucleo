66a8940f8cc34ec5b764d579433fe44f562d6f1f
Alexander Altshuler <altexy@gmail.com>
2012-06-07 23:05:32 +0400

    lua-nucleo/code/profile: remove import_as_require.lua from skip list

--- lua-nucleo/code/profile.lua
+++ lua-nucleo/code/profile.lua
@@ -15,7 +15,6 @@ local PROFILE = { }
 PROFILE.skip = setmetatable(tset
 {
   "lua-nucleo/import.lua";  -- Too low-level
-  "lua-nucleo/import_as_require.lua";  -- Too low-level
   "lua-nucleo/strict.lua";  -- Too low-level
   "lua-nucleo/suite.lua";   -- Too low-level
   "lua-nucleo/table.lua";   -- Contains aliases only, too ambiguous
