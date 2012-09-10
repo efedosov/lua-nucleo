30581fac79ef055324936ac32dec04a70dc9f833
Alexander Altshuler <altexy@gmail.com>
2012-09-11 00:08:56 +0400

    lua-nucleo/init: initial version

---
+++ lua-nucleo/init.lua
@@ -0,0 +1,9 @@
+--------------------------------------------------------------------------------
+-- init.lua: lua-nucleo initialization
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+require 'lua-nucleo.strict'
+require 'lua-nucleo.import'
+require = import 'lua-nucleo/require_and_declare.lua' { 'require_and_declare' }
