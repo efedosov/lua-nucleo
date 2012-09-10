9f0530b0ae414a960694d4ff357b75c80afcb7bd
Alexander Altshuler <altexy@gmail.com>
2012-09-11 00:28:55 +0400

    test/cases/0450-init: initial version

---
+++ test/cases/0450-init.lua
@@ -0,0 +1,13 @@
+--------------------------------------------------------------------------------
+-- 0450-init.lua:
+-- This file is a part of lua-nucleo library
+-- Copyright (c) lua-nucleo authors (see file `COPYRIGHT` for the license)
+--------------------------------------------------------------------------------
+
+require 'lua-nucleo'
+
+-- ensure import is defined
+assert(import ~= nil)
+
+-- ensure strict is loaded
+assert(declare ~= nil)
